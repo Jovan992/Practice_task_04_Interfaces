2020b32074c4834e6077cff6b2e559a7913455bf
Jovan992 <ranisavljev.j@hotmail.com>
2023-12-29 19:07:37 +0100

    Practice task - Interfaces

---
+++ Practice_task_04_Interfaces/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Practice_task_04_Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassA objA = new ClassA();
+            ClassB objB = new ClassB();
+
+            NewMethod(objA);
+            NewMethod(objB);
+
+            Console.ReadKey();
+        }
+
+        public static void NewMethod(IMyInterface a)
+        {
+            a.MyMethod();
+        }
+    }
+    public interface IMyInterface
+    {
+        void MyMethod();
+    }
+
+    public class ClassA : IMyInterface
+    {
+        public void MyMethod()
+        {
+            Console.WriteLine("My method in class A");
+        }
+    }
+
+    public class ClassB : IMyInterface
+    {
+        public void MyMethod()
+        {
+            Console.WriteLine("My method in class B");
+        }
+    }
+}
